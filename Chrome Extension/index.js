525b1e68676b29af3e31c8f1c809d8205c493da9
HAMMAD TARIQUE <mo.hammadtarique1963@gmail.com>
2024-08-15 01:36:39 +0530

    Made the saveTab function running

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -3,24 +3,52 @@ let myLeads = [];
 const inputBar = document.getElementById('input-bar');
 let inputBtn = document.getElementById('input-btn');
 const unLi = document.getElementById('un-li');
+let deleteBtn = document.getElementById('delete-btn');
+let tabBtn = document.getElementById('tab-btn');
 
-inputBtn.addEventListener("click", () => {
-  myLeads.push(inputBar.value);
-  inputBar.value = "";
-  renderLeads();
-});
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
+if(leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
 
-function renderLeads(){
+function render(leads){
   let listItems = "";
-  for(let i = 0; i < myLeads.length; i++) {
+  for(let i = 0; i < leads.length; i++) {
     listItems += `
       <li>
-        <a href="${myLeads[i]}" target="_blank">
-          ${myLeads[i]}
-        </a>
+        <a href="${leads[i]}" target="_blank">
+          ${leads[i]}
+          </a>
       </li>
     `;
   }
   unLi.innerHTML = listItems;
 }
+
+tabBtn.addEventListener("click", () => {
+
+  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
+
+
+deleteBtn.addEventListener("dblclick", () => {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+
+
+inputBtn.addEventListener("click", () => {
+  myLeads.push(inputBar.value);
+  inputBar.value = "";
+
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+  // console.log(localStorage.getItem("myLeads"));
+});
